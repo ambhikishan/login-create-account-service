0b2b5a2b42e7c1446bda7ab3b6dd75110d73afae
ambhikishan <ambhikishan28@outlook.com>
2026-01-08 17:16:44 +0530

    redis secured otp added with encryption

--- src/main/java/com/example/Login/service/RedisOtpService.java
+++ src/main/java/com/example/Login/service/RedisOtpService.java
@@ -2,6 +2,7 @@ package com.example.Login.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
@@ -13,16 +14,20 @@ public class RedisOtpService {
     @Autowired
     private RedisTemplate<String,String> redisTemplate;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     private static final Duration OTP_EXPIRY = Duration.ofMinutes(5);
 
     public void saveOtp(String email, String otp) {
+        otp =bCryptPasswordEncoder.encode(otp);
         redisTemplate.opsForValue().set(email, otp, OTP_EXPIRY);
 
 
     }
 
     public boolean checkOtp(String email, String otp) {
-        boolean otpStatus = redisTemplate.opsForValue().get(email).equals(otp);
+        boolean otpStatus = bCryptPasswordEncoder.matches(otp,redisTemplate.opsForValue().get(email));
         redisTemplate.delete(email);
         return otpStatus;
     }
